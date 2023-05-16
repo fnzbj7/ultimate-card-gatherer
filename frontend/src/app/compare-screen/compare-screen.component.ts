c880a08efecbededbe2cbba26c5e29993a6487e2
Arpas Milan <arpas.milan@eing.tigra.hu>
2023-05-16 08:55:52 +0200

    Small Fix

--- frontend/src/app/compare-screen/compare-screen.component.ts
+++ frontend/src/app/compare-screen/compare-screen.component.ts
@@ -3,6 +3,10 @@ import { CompareCardDto, CompareCardService } from './compare-card.service';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { Store, select } from '@ngrx/store';
+import { TaskService } from '../store/task.service';
+import { AppState } from '../store/task.reducer';
+import { finishTask } from '../store/task.actions';
 
 type BestNum = {
   [key: string]: number;
@@ -30,11 +34,23 @@ export class CompareScreenComponent implements OnInit {
     //private cardUltimateService: CardUltimateService,
     private http: HttpClient,
     private route: ActivatedRoute,
+    private store: Store<AppState>,
+    private taskService: TaskService
   ) {}
 
   ngOnInit() {
     this.compareList = this.compareCardService.compareList;
     this.id = this.route.snapshot.params['id'];
+
+    this.store.pipe(select((state) => state.tasks)).subscribe((tasks) => {
+      if (tasks.jsonBase) {
+        const { jsonBase } = tasks;
+        this.setCode = jsonBase.setCode;
+      }
+    });
+    if (!this.taskService.id && this.id) {
+      this.taskService.setId(+this.id);
+    }
 
     // this.jsonName = this.route.snapshot.params.jsonName;
 
@@ -81,8 +97,6 @@ export class CompareScreenComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.myFormGroup);
-
     this.renameState = 'load';
     this.isRenameLoading = true;
 
@@ -109,7 +123,8 @@ export class CompareScreenComponent implements OnInit {
     }
 
     this.http.post<void>('api/rename', renameDto).subscribe(() => {
-      console.log('végzett a rename');
+      // TODO store
+      this.store.dispatch(finishTask({ taskId: 'isCheckNumberF' }));
       this.isRenameLoading = false;
       this.isRenameFinised = true;
       this.renameState = 'finished';
